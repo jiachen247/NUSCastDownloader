1ad05aa513275e7a4297be2d4dc39621dc795eac
jiachen <jiachen247@gmail.com>
2018-11-25 01:31:35 +0800

    initial

---
+++ src/inject/inject.js
@@ -0,0 +1,31 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+		if (document.readyState === "complete" && document.querySelector('#podcastDownload').getAttribute("style")) {
+			clearInterval(readyStateCheckInterval);
+
+			function isDownloadable(element){
+				return !element.getAttribute("style").includes("hidden");
+			}
+
+			// todo add gh link here to log issues
+			console.log("[+] hello there :-) ");
+
+			/*
+				JQuery Impl
+				$("#podcastDownload").attr({
+					style: "display: inline-block;",
+					target: "_blank",
+					href: $("meta[name='twitter:player:stream']").attr("content")
+				});
+			*/
+			
+			const e = document.querySelector('#podcastDownload');
+			
+			if(!isDownloadable(e)){
+				e.setAttribute("style", "display: inline-block;");
+				e.setAttribute('target', '_blank');
+				e.setAttribute('href', document.querySelector("meta[name='twitter:player:stream']").getAttribute("content"));
+			}
+		}
+	}, 10);
+});
